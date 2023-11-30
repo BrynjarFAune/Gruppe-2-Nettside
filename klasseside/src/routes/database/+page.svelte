dce9f4bf84d091544cb5ffa407f69c0559da3166
Brunjar <brynjarfaune@gmail.com>
2023-11-30 23:30:05 +0100

    Laget Sveltekit 'klasseside', og fikset setup og layout inkludert header + footer

---
+++ klasseside/src/routes/database/+page.svelte
@@ -0,0 +1,10 @@
+<img class="backimg" src="./background-database.png">
+
+<style>
+img {
+    z-index: -1;
+    height: 100%;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+}
+</style>
