9ba3a6ad10b21275f11d2475117fdd5a04a54115
TorjeSJac <147614146+TorjeSJac@users.noreply.github.com>
2023-12-04 13:45:31 +0000

    litt contact

--- klasseside/src/routes/contact/+page.svelte
+++ klasseside/src/routes/contact/+page.svelte
@@ -1,1 +1,34 @@
-<h1>This is contact</h1>
+
+    <div class="Kontakskjema">
+        <label>Name</label>
+        <input type="text" required title="Name">
+        <label>E-post</label>
+        <input type="text" required title="Email">
+        <label>Hva gjelder det?</label>
+        <textarea required id="Hva_gjelder_det" cols="30" rows="10"></textarea>
+        
+        <button>Submit</button>
+    </div>
+
+<style>
+}
+.Kontakskjema{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    margin-left: 500px;
+    margin-right: 500px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: left;
+}
+button{
+    color: black;
+    background-color: white;
+    margin: 7px;
+    border-radius: 3px;
+}
+
+</style>
