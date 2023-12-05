dd29d9711f068bbb3da06aa67783254b66c1bb8c
Brunjar <brynjarfaune@gmail.com>
2023-12-05 22:09:03 +0100

    fixed layout styling

--- klasseside/src/routes/+layout.svelte
+++ klasseside/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 
 <header>
+<div class="content">
+
 <p class="contact">Ring kontaktperson: +47 123 123 123</p>
 <section class="medier">
 <div class="twitter-wrapper">bird</div>
@@ -7,45 +9,117 @@
 <div class="facebook-wrapper"></div>
 <div class="instagram-wrapper"></div>
 </section>
+</div>
 </header>
 
 <nav>
-<a href="/"><span><icon><img src="./logo.png" alt="tromsokommune logo"></icon><h3 class="title">2IT Breivang VGS</h3></span></a>
+<div class="content">
+
+<a href="/" class="logo-link">
+  <span class="logo">
+    <img src="./logo.png" alt="tromsokommune logo">
+  </span>
+  <h3 class="title">2IT Breivang VGS</h3>
+</a>
+
+
 <span class="nav-links">
 <a href="/"><h4 class="link">Hjem</h4></a>
 <a href="/database"><h4 class="link">Database</h4></a>
 <a href="/events"><h4 class="link">Eventer</h4></a>
 <a href="/contact"><h4 class="link">Kontakt</h4></a>
 </span>
+</div>
 </nav>
 
 <slot></slot>
 
 <footer>
-<div class="footer-content">
-<p>© 2023 Breivang VGS. Alle rettigheter forbeholdt.</p>
-<p>Adresse: Dramsvegen 199, 9010 Tromsø | Telefon: 77 78 85 00 | E-post: breivang@tffk.no</p>
-<p>Designet & Utviklet av 2IT</p>
+<div class="content">
+<p class="footer-txt">© 2023 Breivang VGS. Alle rettigheter forbeholdt.</p>
+<p class="footer-txt">Adresse: Dramsvegen 199, 9010 Tromsø | Telefon: 77 78 85 00 | E-post: breivang@tffk.no</p>
+<p class="footer-txt">Designet & Utviklet av 2IT</p>
 </div>
 </footer>
 
 <style>
 
 * {
+    margin: 0;
     display: flex;
-    background-color: #232D3F;
 }
+
+a, a > * {
+    text-decoration: none;
+}
+
+/* Header styling */
 
 header {
     height: 31px;
+    background-color: #C9C9CA;
 }
 
-nav {
-    height: 96px;
+
+/* Navbar styling */
+
+nav { 
+    height: 65px;
+    flex-direction: row;
+    background-color: green;
+    background-color: #1E1E1E;
 }
+
+.nav-links {
+    align-items: center;
+    text-decoration: none;
+}
+
+.logo-link {
+    display: flex;
+    align-items: center;
+}
+
+.logo img {
+    height: 45px;
+    width: auto;
+}
+
+
+
+.title, .link {
+    color: #ffffff;
+    font-size: 16;
+    text-decoration: none;
+    margin-left: 15px;
+}
+
+
+/* Footer styles */
 
 footer {
     height: 67px;
+    background-color: #1E1E1E;
+       
+    /*send to bottom*/
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+}
+
+.content {
+    height: 100%;
+    background-color: inherit;
+    margin: 0 31px;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+}
+.footer-txt {
+    background-color: #1E1E1E;
+    color: #ffffff;
+    font-size: 12;
 }
 
 </style>
