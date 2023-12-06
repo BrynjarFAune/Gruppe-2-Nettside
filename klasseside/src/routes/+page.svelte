2c8ce262be2c4828a08b9c7310b8474eb4ecabc6
lotading <aksel.joakim06@gmail.com>
2023-12-07 00:38:05 +0100

    Arbeidet med forside

--- klasseside/src/routes/+page.svelte
+++ klasseside/src/routes/+page.svelte
@@ -1,6 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/db">Database</a>
+
+<section>
+  <article>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste provident quo voluptatem veritatis dolore, mollitia accusantium sed harum fuga aperiam vitae nobis maiores nulla obcaecati error, inventore saepe nostrum.
+  </article>
+
+  <img src="../background-main.png" alt="tromsokommune logo">
+
+ 
+
+
+</section>
+
 
 <!-- src/routes/page1.svelte -->
 <script context="module">
@@ -9,3 +19,44 @@
 
 <!-- Rest of the component -->
 
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+section {
+  display: flex;
+  justify-content: right;
+  top:0;
+  
+}
+
+section img {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  z-index: -2;
+}
+
+article {
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  color: white;
+  z-index: -1;
+  width: 30%;
+  margin: 0 auto;
+  transform: translateX(50%); 
+  display: flex;
+  
+  
+  @media (max-width: 600px) {
+    width: 80%;
+    transform: translateX(0);
+  }
+}
+
+</style>
